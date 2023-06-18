91e1bb8ac6cdcdc138afa374afe1f96f979147bc
helmihd <helmihd02@gmail.com>
2023-06-19 03:56:11 +0700

    Update

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet" />
+        <link href="./css/login.css" rel="stylesheet" />
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="header">
+            <table class="header-box">
+                <tr>
+                    <td class="logo-text">DejaVu C<img src="./assets/logo.png" class="logo-nav-bar">offee</td>
+                    <td class="login-button">LOGIN</td>
+                    <td class="home-button">HOME</td>
+                    <td class="about-button">ABOUT</td>
+                    <td class="contact-button">CONTACT</td>
+                    <td class="logo-chart-box"><img src="./assets/shopping-cart.png" class="logo-chart"></td>
+                </tr>
+            </table>
+            <div class="nav-bar-pic-box"><img src="./assets/nav-bar_pic.png" class="nav-bar-pic"></div>
+        </div>
+        <div class="login-blank">
+            <div class="login-box">
+                <div class="login-text">Login</div>
+                <form>
+                    <div class="username-text">Username or email address *</div>
+                    <input class="input-email" type="text" placeholder="Username or Email">
+                    <div class="password-text">Password *</div>
+                    <input class="input-password" type="password" placeholder="Password">
+                    <input class="input-button" type="submit" value="Log in">
+                    <label for="remember-me-box" class="remember-me-text">Remember me</label>
+                    <input id="remember-me-box" class="remember-me-box" type="checkbox">
+                </form>
+                <div class="link-register">Don't have an account?</div>
+            </div>
+        </div>
+        <div class="footer">
+            <table class="social-media-box">
+                <tr>
+                    <td colspan="3" class="social-media-text">Our Social Media</td>
+                </tr>
+                <tr class="Social-media-logo">
+                    <td><img src="./assets/logo_whatsapp.png" class="whatsapp-logo"></td>
+                    <td><img src="./assets/logo_instagram.png" class="instagram-logo"></td>
+                    <td><img src="./assets/logo_twitter.png" class="twitter-logo"></td>
+                </tr>
+                <tr class="Social-media-text">
+                    <td class="whatsapp-text">+62 82123456789</td>
+                    <td class="instagram-text">@DejaVuCoffee</td>
+                    <td class="twitter-text">@DejaVuCoffee</td>
+                </tr>
+            </table>
+            <table class="copyright-box">
+                <tr>
+                    <td class="copyright-text">Copyright © 2023 | Rekayasa Perangkat Lunak</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
