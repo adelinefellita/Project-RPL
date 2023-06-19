57329646c2952cb163f7955a29fc5aab38ef252f
helmihd <helmihd02@gmail.com>
2023-06-19 14:41:13 +0700

    Update

---
+++ outlet_berdasarkan_kota.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet" />
+        <link href="./css/outlet_berdasarkan_kota.css" rel="stylesheet" />
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="header">
+            <table class="header-box">
+                <tr>
+                    <td class="logo-text">DejaVu C<img src="./assets/logo.png" class="logo-nav-bar">offee</td>
+                    <td class="login-button">LOGIN</td>
+                    <td class="home-button">HOME</td>
+                    <td class="about-button">ABOUT</td>
+                    <td class="contact-button">CONTACT</td>
+                    <td class="logo-chart-box"><img src="./assets/shopping-cart.png" class="logo-chart"></td>
+                </tr>
+            </table>
+            <div class="nav-bar-pic-box"><img src="./assets/nav-bar_pic.png" class="nav-bar-pic"></div>
+        </div>
+        <div class="container">
+            <div class="city-name">Jakarta</div>
+            <hr>
+            <table class="outlet-table" cellspacing="10">
+                <tr>
+                    <td>
+                        <table class="content-table" >
+                            <tr>
+                                <td class="outlet-name">DejaVu Coffee Tebet</td>
+                                <td rowspan="3"><img src="./assets/Store-1.png" class="store-pic"></td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-address">MT Haryono St No.9, RT.11/RW.5, West Tebet, Jakarta, South Jakarta City, Jakarta 12810</td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-status">Open - Close 22.00</td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>
+                        <table class="content-table" >
+                            <tr>
+                                <td class="outlet-name">DejaVu Coffee Gubeng</td>
+                                <td rowspan="3"><img src="./assets/Store-2.png" class="store-pic"></td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-address">Jl. Raya Gubeng No.33, RT.002/RW.06, Gubeng, Kec. Gubeng, Surabaya, Jawa Timur 60281</td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-status">Closed - Open 08.00</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="content-table" >
+                            <tr>
+                                <td class="outlet-name">DejaVu Coffee Slamet Riyadi</td>
+                                <td rowspan="3"><img src="./assets/Store-3.png" class="store-pic"></td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-address">Slamet Riyadi St No.301, Penumping, Laweyan, Surakarta City, Central Java 57141</td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-status">Closed - Open 08.00</td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>
+                        <table class="content-table" >
+                            <tr>
+                                <td class="outlet-name">DejaVu Coffee Madiun</td>
+                                <td rowspan="3"><img src="./assets/Store-4.png" class="store-pic"></td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-address">Jl. Cokroaminoto No.40, Kejuron, Kec. Taman, Kota Madiun, Jawa Timur 63133</td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-status">Closed - Open 08.00</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table class="content-table" >
+                            <tr>
+                                <td class="outlet-name">DejaVu Coffee Malioboro</td>
+                                <td rowspan="3"><img src="./assets/Store-5.png" class="store-pic"></td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-address">Jl. Malioboro No.41, Sosromenduran, Gedong Tengen, Daerah Istimewa Yogyakarta 55271</td>
+                            </tr>
+                            <tr>
+                                <td class="outlet-status">Closed - Open 08.00</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="gap"></div>
+        <div class="footer">
+            <table class="social-media-box">
+                <tr>
+                    <td colspan="3" class="social-media-text">Our Social Media</td>
+                </tr>
+                <tr class="Social-media-logo">
+                    <td><img src="./assets/logo_whatsapp.png" class="whatsapp-logo"></td>
+                    <td><img src="./assets/logo_instagram.png" class="instagram-logo"></td>
+                    <td><img src="./assets/logo_twitter.png" class="twitter-logo"></td>
+                </tr>
+                <tr class="Social-media-text">
+                    <td class="whatsapp-text">+62 82123456789</td>
+                    <td class="instagram-text">@DejaVuCoffee</td>
+                    <td class="twitter-text">@DejaVuCoffee</td>
+                </tr>
+            </table>
+            <table class="copyright-box">
+                <tr>
+                    <td class="copyright-text">Copyright © 2023 | Rekayasa Perangkat Lunak</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
